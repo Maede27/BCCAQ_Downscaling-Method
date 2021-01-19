979d3c1c2550f568f5f75563bc598da71293e4b5
Maya Nouri <maede.nouri27@gmail.com>
2021-01-18 21:54:34 -0500

    Update R_climdown_CanESM5.R

--- R_climdown_CanESM5.R
+++ R_climdown_CanESM5.R
@@ -1,7 +1,7 @@
 library(ncdf4)
 library(ClimDown)
-GCM<-"/home/najafim/scratch/lake_winnipeg/GCMs/Test_Obs_GCM/Test_GCM_1950-2100.$
-Obs<-"/home/najafim/scratch/lake_winnipeg/GCMs/Test_Obs_GCM/Test_Obs_newLon_sam$
+GCM<-"/home/najafim/scratch/lake_winnipeg/GCMs/Test_Obs_GCM/Test_GCM_1950-2100.nc
+Obs<-"/home/najafim/scratch/lake_winnipeg/GCMs/Test_Obs_GCM/Test_Obs_newLon_sameVar_sameUnit.nc"
 GCM_open<-nc_open(GCM)
 print(GCM_open)
 
